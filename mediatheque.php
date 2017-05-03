ffa924ae84e71e7085a34b6c376725a058aac34c
imath <math.viet@gmail.com>
2017-05-03 09:54:03 +0200

    Force the plugin to be activated on the network

--- mediatheque.php
+++ mediatheque.php
@@ -1,17 +1,18 @@
 <?php
-/*
-Plugin Name: MediaThèque
-Plugin URI: https://imathi.eu/tag/mediatheque/
-Description: Une gestion alternative des media dans WordPress, pour tous.
-Version: 1.0.0
-Requires at least: 4.7
-Tested up to: 4.7
-License: GNU/GPL 2
-Author: imath
-Author URI: https://imathi.eu/
-Text Domain: mediatheque
-Domain Path: /languages/
-*/
+/**
+ * Plugin Name: MediaThèque
+ * Plugin URI: https://imathi.eu/tag/mediatheque/
+ * Description: Une gestion alternative des media dans WordPress, pour tous.
+ * Version: 1.0.0
+ * Requires at least: 4.7
+ * Tested up to: 4.7
+ * License: GNU/GPL 2
+ * Author: imath
+ * Author URI: https://imathi.eu/
+ * Text Domain: mediatheque
+ * Domain Path: /languages/
+ * Network: True
+ */
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
